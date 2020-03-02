af9761c7accd9a1688f96b6fe2c28d167333a60f
aya <56827395+almayra@users.noreply.github.com>
2020-03-02 14:17:28 +0700

    uwow

--- controller/productController.js
+++ controller/productController.js
@@ -50,5 +50,44 @@ module.exports={
         } catch (error) {
             res.send(error)
         }
+    },
+    getKelas:(req, res)=>{
+        // var sql='SELECT p.id,p.judul,p.deskripsi,p.cover,p.bab,p.materi FROM product p join category c on p.idkategori=c.id;'
+        var sql='SELECT * FROM product p join category c on p.idkategori=c.idkat '
+        mysql.query(sql, (err, res1)=>{
+            if(err){
+                return res.status(500).send(err)
+            }
+            return res.status(200).send(res1)
+        })
+    },
+    deleteKelas: (req, res)=>{
+        console.log('delete')
+        var selectedId=req.params.selectedId
+        console.log('id deletenye',selectedId)
+        var sql=`SELECT * FROM product WHERE id=${selectedId}`
+        mysql.query(sql, (err,results1)=>{
+            if(err){
+                console.log('problem', err)
+                return res.status(500).send(err)
+            }
+            console.log(results1)
+            console.log('alamat', results1[0].cover)
+            var imagePath=results1[0].cover
+            sql=`DELETE FROM product WHERE id=${selectedId}`
+            mysql.query(sql, (err, results)=>{
+                if(err){
+                    console.log(err.message)
+                    return res.status.json({message:`there's an error on the server`, err:err.message})
+                }if(imagePath){
+                    fs.unlinkSync(`./public`+imagePath)
+                }
+                console.log(results)
+                mysql.query(sql, (err, result2)=>{
+                    if(err) res.status(500).send(err)
+                    res.status(200).send({dataProduct:result2})
+                })
+            })
+        })
     }
 }
--- router/KelasRouter.js
+++ router/KelasRouter.js
@@ -3,5 +3,7 @@ const {productController}=require('../controller')
 const router=express.Router()
 
 router.post('/tambahkelas', productController.postKelas)
+router.get('/getkelas', productController.getKelas)
+router.delete('/deletekelas/:selectedId', productController.deleteKelas)
 
 module.exports=router
